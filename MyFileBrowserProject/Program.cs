6e01b30c1e1e4d5a2654ff42ef2e1b6945ab48cc
Marcin Kostulski <marcin.kostulski@immalle.eu>
2017-10-27 11:15:50 +0200

    Base

--- MyFileBrowserProject/Program.cs
+++ MyFileBrowserProject/Program.cs
@@ -10,6 +10,31 @@ namespace MyFileBrowserProject
     {
         static void Main(string[] args)
         {
+            if( args.Length == 0 )
+            {
+                Console.WriteLine("Geef een argument");
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case "-f":
+                        Console.WriteLine("Alle files in de huidige directory");
+                        break;
+                    case "-c":
+                        Console.WriteLine("Huidige directory");
+                        break;
+                    case "-d":
+                        Console.WriteLine("Alle subdirectories in de huidige directory");
+                        break;
+                    case "-h":
+                        Console.WriteLine("Toont help");
+                        break;
+                    default:
+                        Console.WriteLine("Onbekend argument");
+                        break;
+                }
+            }
         }
     }
 }
